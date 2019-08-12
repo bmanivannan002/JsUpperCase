63feeb1ea4a0f69742db05db0a5dbfc087cecc87
Balakrishnan <manivannan.balakrishnan@capgemini.com>
2019-08-12 20:38:33 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function JsUpperCase()
+{
+    let result =[];
+    if(arguments.length == 0) 
+    {
+        console.log('Argument is missing'); 
+    }
+    else if(arguments.length == 1) 
+    {
+        result.push(arguments[0].toLocaleUpperCase());
+    }
+    else
+    {
+        let arg = Array.prototype.slice.call(arguments);
+        arg.forEach(element => {
+            result.push(element.toLocaleUpperCase());
+        });
+        
+    }
+    return result;
+}
+console.log('Conversion completed successfully');
