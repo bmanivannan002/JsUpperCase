e2bce77a42656111f2d98d20da70e968316c3c7b
Balakrishnan <manivannan.balakrishnan@capgemini.com>
2019-08-27 11:58:06 +0200

    changing the structure

--- jsuppercase.js
+++ jsuppercase.js
@@ -2,34 +2,28 @@
  * JsUppercase
 
  */
-
-function jsUpperCase()
-{
-    let result =[];
-    if(arguments.length == 0) 
-    {
-        console.log('Argument is missing'); 
+var JSUpperCase = {
+    jsUpperCase: function () {
+        let result = [];
+        if (arguments.length == 0) {
+            console.log('Argument is missing');
+        }
+        else if (arguments.length == 1) {
+            result.push(arguments[0].toLocaleUpperCase());
+        }
+        else {
+            let arg = Array.prototype.slice.call(arguments);
+            arg.forEach(element => {
+                if (typeof element == "string") {
+                    result.push(element.toLocaleUpperCase());
+                }
+                else {
+                    result.push(element);
+                }
+            });
+        }
+        console.log(result);
+        return result;
     }
-    else if(arguments.length == 1) 
-    {
-        result.push(arguments[0].toLocaleUpperCase());
-    }
-    else
-    {
-        let arg = Array.prototype.slice.call(arguments);
-        arg.forEach(element => {
-            if(typeof element =="string")
-            {
-                result.push(element.toLocaleUpperCase());
-            }
-            else
-            {
-                result.push(element);  
-            }
-        });
-    }
-    console.log(result);
-    return result;
 }
-
-module.exports = jsUpperCase;
+module.exports = JSUpperCase;
